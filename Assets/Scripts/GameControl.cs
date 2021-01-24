9efc3fc9b086d32d1017b5f1f98c8ebb404c7e3d
Will Cosker <wcosker@ufl.edu>
2021-01-24 13:12:25 -0500

    init

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+  //*************************************
+    public static GameControl control;//*
+  //*************************************
+    // ^^^^ This is the object that stores ALL of the players dynamic data
+    //used for upgrades etc
+
+    public int maxUses;//these are the saved player values
+    public float moveSpeed;
+    public float wateringTime;
+    public int waterAllUses;
+    public AudioMixer mixer;
+
+    void Awake()//if DDOL object already exists, destroy yourself bro :)
+    {
+        if(control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()//set up music and all player prefs etc etc
+    {
+        mixer.SetFloat("musicVol", PlayerPrefs.GetFloat("musicVol", 0));
+        mixer.SetFloat("fxVol", PlayerPrefs.GetFloat("fxVol", 0));
+    }
+
+    public void Save()//saves player data to a file (data.swag)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/data.swag";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        GameData data = new GameData();
+
+        data.maxUses = maxUses;
+        data.moveSpeed = moveSpeed;
+        data.wateringTime = wateringTime;
+        data.waterAllUses = waterAllUses;
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public void Load()//loads player data from file
+    {
+        string path = Application.persistentDataPath + "/data.swag";
+        if (File.Exists(path))//if file is found input data into the local DDOL "control" object
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+
+            stream.Close();
+
+            maxUses = data.maxUses;
+            moveSpeed = data.moveSpeed;
+            wateringTime = data.wateringTime;
+            waterAllUses = data.waterAllUses;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+        }
+    }
+
+    public void musicVolume(float volume)
+    {
+        mixer.SetFloat("musicVol", volume);
+        PlayerPrefs.SetFloat("musicVol", volume);
+    }
+
+    public void effectsVolume(float volume)
+    {
+        mixer.SetFloat("fxVol", volume);
+        PlayerPrefs.SetFloat("fxVol", volume);
+    }
+
+    public void goToNewScene(String sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
+
+[Serializable]
+class GameData //THIS HOLDS RAW PLAYER DATA
+{
+    public float moveSpeed;
+    public float wateringTime;
+    public int maxUses;
+    public int waterAllUses;
+}
