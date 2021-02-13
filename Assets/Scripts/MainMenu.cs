3eef4453823859ff77db04ad108ba735b1bcb84e
Schuyler Gleaves <Schuylergleaves@ufl.edu>
2021-02-13 16:56:54 -0500

    Added MainMenu and Settings scenes, moved old scenes to Debug folder

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadMainMenuScene()
+    {
+        Application.LoadLevel(0);
+    }
+    
+    public void LoadSettingsScene() 
+    {
+        Application.LoadLevel(1);
+    }
+}
