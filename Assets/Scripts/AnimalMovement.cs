56c8dc89c2b07ac30e596a9277aba681c9c701f6
William Cosker <wcosker@ufl.edu>
2021-03-15 22:41:23 -0400

    added zebra model and debug animation scene
    going to make new branch to add model and animation to the map scene. this branch was merely for getting the animation inside of unity and having it work.

---
+++ Assets/Scripts/AnimalMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalMovement : MonoBehaviour
+{
+    private Animator animator;
+    private Vector3 moveTowards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //Every ten seconds this function is called to change the walking direction of animal
+        InvokeRepeating("getNewMovePos",0,11f);
+    }
+
+    void getNewMovePos()
+    {
+        //randomizes a new  Vector3 in specific range
+        moveTowards = new Vector3(Random.Range(-5f,5f),transform.position.y,Random.Range(-5f,5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the distance from each vector3 is significant
+        if (Vector3.Distance(moveTowards,transform.position) > 1f)
+        {
+            //start rotating towards new position and moving towards, based on time deltaTime
+            transform.position = Vector3.MoveTowards(transform.position,moveTowards,Time.deltaTime/1.5f);
+            Vector3 newRot = Vector3.RotateTowards(transform.forward,moveTowards,Time.deltaTime*3,0);
+            newRot = new Vector3(newRot.x,0,newRot.z);
+            transform.rotation = Quaternion.LookRotation(newRot);
+
+            //begin animation
+            animator.SetBool("isWalking",true);
+
+        }
+        else animator.SetBool("isWalking",false);
+    }
+}
