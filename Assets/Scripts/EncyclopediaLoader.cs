aa379fbef60a83a2a4b70400aff3ee676cb01a52
RACZ\13054 <qaqsd123@gmail.com>
2021-03-23 21:11:13 -0400

    fixed encyclopedia

--- Assets/Scripts/EncyclopediaLoader.cs
+++ Assets/Scripts/EncyclopediaLoader.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class EncyclopediaLoader : MonoBehaviour
 {
     private AnimalDataHandler animalDataHandler;
     public GameObject listItemPrefab;
-    public GameObject cardPrefab;
     public GameObject ContentList;
     public GameObject MainPanel;
-    private GameObject card;
+    public GameObject card;
     // Start is called before the first frame update
     void Start()
     {
-        animalDataHandler = new AnimalDataHandler();
+        animalDataHandler = new AnimalDataHandler();    //probably shouldnt instantiate different data handlers per scene 
         animalDataHandler.LoadAnimalsIntoList();
-        // animalDataHandler.PrintAnimalList();
+        // animalDataHandler.PrintAnimalList(); 
         LoadAnimalsToEncyclopedia();
-        MainPanel.GetComponent<Button>().onClick.AddListener(()=>Destroy(card));
+        MainPanel.GetComponent<Button>().onClick.AddListener(()=>{card.active = false;});   //for hiding card 
 
     }
 
@@ -31,39 +31,49 @@ public class EncyclopediaLoader : MonoBehaviour
     public void LoadAnimalsToEncyclopedia() {
 
         foreach(Animal animal in animalDataHandler.animalList.Animals) {
-            GameObject listItem = Instantiate (listItemPrefab, ContentList.transform);
-            Image imageSpot = listItem.transform.Find("ImagePanel").gameObject.GetComponent<Image>();
-            StartCoroutine(setImage(animal.ImageURL, imageSpot)); //balanced parens CAS
-            Text ButtonText = GameObject.Find("Name").GetComponent<Text>();
-            ButtonText.text = "test";
-            listItem.GetComponent<Button>().onClick.AddListener(()=>cardPopup(imageSpot.sprite, animal.CommonName, animal.Description));
+            GameObject listItem = Instantiate (listItemPrefab, ContentList.transform);  //button prefab
+            RawImage raw = listItem.transform.Find("ImagePanel").Find("Raw").gameObject.GetComponent<RawImage>();   //finding image gameobject
+            StartCoroutine(setButtonImage(animal.ImageURL, raw, listItem, animal));     //using coroutines for loading images. might want to use async ops in the future
+            Text ButtonText = listItem.transform.Find("Name").gameObject.GetComponent<Text>();  
+            ButtonText.text = animal.CommonName;
         }
     }
 
-    IEnumerator setImage(string url, Image card) {
-        WWW www = new WWW(url);
-        yield return www;
+    IEnumerator setButtonImage(string url, RawImage raw, GameObject button, Animal animal)
+    {
+        using(UnityWebRequest request = UnityWebRequestTexture.GetTexture(url)) 
+        {
+            yield return request.SendWebRequest();
 
-        // calling this function with StartCoroutine solves the problem
-        Debug.Log("Why on earh is this never called?");
-        card.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
-        www.Dispose();
-        www = null;
+            if (request.isNetworkError)
+            {
+                Debug.Log(request.error);
+            }
+            else 
+            {
+                raw.texture = (Texture)((DownloadHandlerTexture) request.downloadHandler).texture;  //sets texture for image in the list
+                button.SetActive(true);
+                button.GetComponent<Button>().onClick.AddListener(()=>cardPopup(raw, animal.CommonName, animal.Description));   //button has to be active to add listener. WHY!?!?!
+
+            }
+        }
     }
 
-    public void cardPopup(Sprite image, string name, string desc) {
+    /*
+    function to have popup for more info
+    basically just hides and unhides the card in front of the list
+    */
+    public void cardPopup(RawImage raw, string name, string desc) {
+        Transform inner = card.transform.Find("Inner");
+        RawImage imageSpot = inner.Find("ImagePanel").Find("Raw").gameObject.GetComponent<RawImage>();
+        imageSpot.texture = raw.texture;
+        
+        Text nameSpot = inner.Find("NamePanel").Find("Name").gameObject.GetComponent<Text>();
+        nameSpot.text = name;
+        
+        Text descSpot = inner.Find("DecriptionPanel").Find("Description").gameObject.GetComponent<Text>();
+        descSpot.text = desc;
 
-        foreach(Animal animal in animalDataHandler.animalList.Animals) {
-            card = Instantiate (cardPrefab, MainPanel.transform);
-            Transform inner = card.transform.Find("Inner");
-            Image imageSpot = inner.Find("ImagePanel").gameObject.GetComponent<Image>();
-            imageSpot.sprite = image;
-            
-            Text nameSpot = inner.Find("NamePanel").Find("Name").gameObject.GetComponent<Text>();
-            nameSpot.text = name;
-            
-            Text descSpot = inner.Find("DecriptionPanel").Find("Description").gameObject.GetComponent<Text>();
-            descSpot.text = desc;
-        }
+        card.active = !(card.active);
     }
 }
